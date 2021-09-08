b50cb4e7d32d570b763686e5229e4b3ee3b4fabb
ellenholmsten <elle6717@stud.kea.dk>
2021-09-08 13:49:25 +0200

    javascript first try

---
+++ main.js
@@ -0,0 +1,58 @@
+const url =
+  "https://semester2-9155.restdb.io/rest/wines?apikey=61362a9343cedb6d1f97ed5c";
+
+//api key
+const options = {
+  headers: {
+    "x-apikey": "61362a9343cedb6d1f97ed5c",
+  },
+};
+
+fetch(url, options)
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+  })
+
+  .then((data) => {
+    //we have the data
+    console.log(data);
+    //handleData(data);
+  })
+
+  .catch((e) => {
+    //woops, something went wrong
+    console.error("an error occured:"), e.message;
+  });
+
+function showProduct(product) {
+  console.log(product);
+
+  //breadcrumbs
+  document.querySelector(".breadcrumbs .brand").textContent = product.colour;
+  document.querySelector(".breadcrumbs .productname").textContent =
+    product.name;
+  //name
+  document.querySelector(".name").textContent = product.name;
+  //year
+  document.querySelector(".year").textContent = product.year;
+  //country
+  document.querySelector(".subtle").textContent = product.region;
+  //grape
+  document.querySelector(".grape").textContent = product.grape;
+
+  //section class info part //
+  //percentage//
+  document.querySelector(".info .percentage").textContent = product.percentage;
+  //color//
+  document.querySelector(".info .color").textContent = product.colour;
+  //price//
+  document.querySelector(".price").textContent = product.price;
+
+  //image
+  document.querySelector(
+    ".smallProduct .image"
+  ).src = `https://semester2-9155.restdb.io/rest/wines?apikey=/${product.id}.webp`;
+}
